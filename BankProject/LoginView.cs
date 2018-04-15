b67ffee39cf44ca83e41f236a6f43a8a2f5bd146
BabakSamimi <babak.nti@gmail.com>
2018-04-15 20:59:34 +0200

    Added files

---
+++ BankProject/LoginView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankProject
+{
+    class LoginView : Form
+    {
+        RichTextBox userNameField;
+        RichTextBox passwordField;
+
+        Button logInButton;
+
+        public RichTextBox UserNameField { get; set; }
+        public RichTextBox PasswordField { get; set; }
+
+        public LoginView()
+        {
+            
+        }
+
+        public void LogIn(Object sender, System.EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ BankProject/View.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace BankProject
+{
+    public abstract class View : Form
+    {
+        int height, width;
+        Color background;
+
+
+        protected View()
+        {
+            height = 550;
+            width = 660;
+            background = Color.FromArgb(115, 172, 150);
+        }
+
+
+
+    }
+}
